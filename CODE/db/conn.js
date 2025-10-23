76789ca4c98fa746fed9a91f12bfcd28cb88dac2
matheustm29 <matheusferreiraanjos@alunos.utfpr.edu.br>
2025-10-23 12:46:54 -0300

    fix images/css

--- CODE/db/conn.js
+++ CODE/db/conn.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('toughts2', 'root','utfpr', {
+const sequelize = new Sequelize('toughts2', 'root', '',{
     host: 'localhost',
     dialect: 'mysql',
 })
