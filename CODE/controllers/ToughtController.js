55c868088ef3b8905b5a95a0754fae17ef20e960
Henrique Menck <hmenck@hotmail.com>
2025-11-17 23:22:22 -0300

    Edição de ideias e rotas da Home

--- CODE/controllers/ToughtController.js
+++ CODE/controllers/ToughtController.js
@@ -3,7 +3,14 @@ const User = require('../models/User');
 
 module.exports = class ToughtsController {
     static async showToughts(req, res) {
-        res.render('toughts/home')
+
+      const toughtsData = await Tought.findAll({
+        include: User,
+      })
+
+      const toughts = toughtsData.map((result) => result.get({plain: true}))
+
+      res.render('toughts/home', {toughts})
     }   
 
     static async dashboard(req, res){
@@ -23,7 +30,12 @@ module.exports = class ToughtsController {
       }
 
       const toughts = user.Toughts.map((result) => result.dataValues)
-        res.render('toughts/dashboard', {toughts})
+      let emptyToughts = false
+
+      if(toughts.length === 0){
+        emptyToughts = true
+      }
+      res.render('toughts/dashboard', {toughts, emptyToughts})
     }
 
     static async createTought(req, res){
@@ -60,6 +72,30 @@ module.exports = class ToughtsController {
           res.redirect('/toughts/dashboard')
         })
         
+      }catch(error){
+        console.log('Aconteceu um erro: ' + error)
+      }
+    }
+    static async updateTought(req, res){
+      const id = req.params.id
+      const tought = await Tought.findOne({where: {id: id}, raw: true})
+      res.render('toughts/edit', {tought})
+    }
+
+    static async updateToughtSave(req, res){
+      const id = req.body.id
+
+      const tought = {
+        title: req.body.title
+      }
+
+      try{
+        await Tought.update(tought, {where: {id: id}})
+
+        req.flash('message', 'Pensamento atualizado com sucesso!')
+        req.session.save(() =>{
+          res.redirect('/toughts/dashboard')
+        })
       }catch(error){
         console.log('Aconteceu um erro: ' + error)
       }
